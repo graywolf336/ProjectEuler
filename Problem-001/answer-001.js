af65f01c7a2e81ca1aab943aca22dae168261c74
graywolf336 <graywolf336@craftyn.com>
2016-03-31 23:01:35 -0500

    Rename the folder and answer

--- Problem-001/answer-001.js
+++ Problem-001/answer-001.js
@@ -1,9 +1,9 @@
-var min = 1;
-var limit = 1000;
-var multipulesOf = [3, 5];
-var multipules = [];
-var sum = undefined;
-for(var i = min; i < limit; i++) {
+let min = 1;
+let limit = 1000;
+let multipulesOf = [3, 5];
+let multipules = [];
+let sum = undefined;
+for(let i = min; i < limit; i++) {
 	multipulesOf.forEach(m => {
 		if(i % m === 0 && !multipules.includes(i)) {
 			multipules.push(i);
